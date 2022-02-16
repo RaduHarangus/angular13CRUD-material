d4ddc0a47c83d2d0212dbdfe17a975bf8a390f05
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-02-16 10:27:00 +0200

    add table from material for productsList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { DialogComponent } from "./components/dialog/dialog.component";
 import { ApiService } from "./services/api.service";
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +13,17 @@ import { ApiService } from "./services/api.service";
 })
 export class AppComponent {
 
-  productList!: [];
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor (private dialog: MatDialog,
                private api: ApiService) {}
 
   ngOnInit(): void {
     this.getAllProducts();
-    console.log(this.productList);
   }
 
   openDialog() {
@@ -30,11 +36,22 @@ export class AppComponent {
     this.api.getProducts()
       .subscribe({
         next: (res) => {
-          this.productList = res;
+          this.dataSource = new MatTableDataSource(res);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
         },
-        error: () => {
-          alert('There was an error trying to fetch the products!');
+        error: (err) => {
+          alert('There was an error trying to fetch the products!' + err);
         }
       })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
